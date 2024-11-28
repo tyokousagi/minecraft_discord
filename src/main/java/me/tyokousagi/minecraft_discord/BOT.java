e07359ab2cda565dc6a1acc67a06d29aa35eb5d2
tyokousagi <tyokousagi20022@gmail.com>
2024-11-28 21:16:03 +0900

    Initial commit

---
+++ src/main/java/me/tyokousagi/minecraft_discord/BOT.java
@@ -0,0 +1,2 @@
+package me.tyokousagi.minecraft_discord;public class BOT {
+}
---
+++ src/main/java/me/tyokousagi/minecraft_discord/Minecraft_discord.java
@@ -0,0 +1,10 @@
+package me.tyokousagi.minecraft_discord;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Minecraft_discord implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
